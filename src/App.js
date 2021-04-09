57d2ed89f009822523e3c6abae92cf0cfc30b024
Jhon A. Romero Gonzaga <romerogonzaga1@gmail.com>
2021-04-09 10:25:53 -0500

    react-router implementado exitosamente

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import { Quiz } from './pages/Quiz'
+import { Quiz } from "./pages/Quiz";
+import { Home } from "./pages/Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <>
-    <Quiz />
-    </>
-  )
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route exact path="/quiz">
+          <Quiz />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
-export default App
+export default App;
